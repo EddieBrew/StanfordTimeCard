721d81bce242e2f998cf9f094feb67062929cb52
robertbrewer <robertbrewer@robertbrewer-HP-Pavilion-Desktop-PC-570-p0xx>
2025-11-03 21:35:31 -0800

    Original Commit

---
+++ StanfordTimeCard/src/main/java/com/StanfordTimeCard/StanfordTimeCard/App.java
@@ -0,0 +1,10 @@
+package com.StanfordTimeCard.StanfordTimeCard;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
